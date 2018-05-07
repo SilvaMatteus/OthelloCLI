e28bdbfef0b6c080033b753a393cf843f000bebe
Matteus Silva <matteus.silva@ccc.ufcg.edu.br>
2018-05-07 16:48:34 -0300

    Display art.

--- C++/othello.cpp
+++ C++/othello.cpp
@@ -1,20 +1,9 @@
 #include <stdio.h>
+#include "display_board.hpp"
 
-#define WHITE_PIECE  'O'
-#define BLACK_PIECE  'X'
-#define LINE_SIZE      8
-#define COLUMN_SIZE    8
+#define MAX_LINE_INDEX      7
+#define MAX_COLUMN_INDEX    7
 
 int main() {
-    for ( int i = 0; i <= LINE_SIZE; i++ ) {
-        for ( int j = 0; j <= COLUMN_SIZE; j++ ) {
-            if ( i % 2 == 0 ) {
-                printf( " %c ", BLACK_PIECE );
-            } else {
-                printf( " %c ", WHITE_PIECE );
-            }
-            
-        }
-        printf( "\n" );
-    }
+    print_display_board();
 }
