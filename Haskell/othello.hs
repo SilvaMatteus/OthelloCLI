2c418246b67735a2a9423aa795937b7cc027bc14
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2018-06-02 23:30:00 -0300

    add isValidMove andpossibleMoves check

--- Haskell/othello.hs
+++ Haskell/othello.hs
@@ -27,4 +27,18 @@ generateAllPositions = [(x, y) | x <- [0..7], y <- [0..7]]
 -- generate empty board
 emptyBoard = Data.Map.fromList (zip generateAllPositions (repeat Empty))
 
+--sum positions pairs
+sumPosition :: Position -> Position -> Position
+sumPosition (positionX1, positionY1) (positionX2, positiony2) = (positionX1 + positionX2, positionY1 + positiony2)
+
+-- verify possible moves using lambda expression and foldr
+possibleMoves color board = foldr
+	(\position possiblePositions -> if isLegalMove color board position then position : possiblePositions else possiblePositions)
+	[]
+	generateAllPositions
+
+-- verify if a movement is valid
+isValidMove :: Piece -> Board -> Position -> Bool
+isValidMove color board position = (changedPiecesOfMove color board position) /= []
+
 main = return ()
